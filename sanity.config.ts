8ea270de5a979ebfd368043da57c00471e527aff
icepcp <69038584+icepcp@users.noreply.github.com>
2024-01-07 00:39:20 +0800

    v1.0.4: fix env var

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,10 +6,10 @@ import {media} from 'sanity-plugin-media'
 
 export default defineConfig({
   name: 'default',
-  title: process.env.SANITY_STUDIO_PROJ_TITLE,
+  title: process.env.SANITY_STUDIO_PROJ_TITLE || "gemtology",
 
   // @ts-ignore
-  projectId: process.env.SANITY_STUDIO_PROJ_ID,
+  projectId: process.env.SANITY_STUDIO_PROJ_ID || "a7vke4vt",
   dataset: 'production',
 
   plugins: [deskTool(), media(), visionTool()],
