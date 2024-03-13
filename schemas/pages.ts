948329ea8296e40ec6448e215bf6f55bcc9aa187
Aung Kaung Myat <69038584+icepcp@users.noreply.github.com>
2024-03-13 13:24:28 +0000

    v1.1.0: add accordion, update deps and remove page title

--- schemas/pages.ts
+++ schemas/pages.ts
@@ -6,15 +6,8 @@ export default {
   title: 'Pages',
   fields: [
     {
-      name: 'name',
-      type: 'string',
-      title: 'Name',
-      description: 'This is the name of the document, it does not matter what you set it as.',
-      validation: (Rule: { required: () => any; }) => Rule.required()
-    },
-    {
       name: 'title',
-      type: 'text',
+      type: 'string',
       title: 'Title',
       rows: 2,
       validation: (Rule: { required: () => any; }) => Rule.required()
--- schemas/portable.ts
+++ schemas/portable.ts
@@ -15,6 +15,26 @@ export default {
           title: 'Alt Text',
         }
       ],
-    }
+    },
+    {
+      name: 'accordion',
+      title: 'Accordion',
+      type: 'object',
+      fields: [
+        {
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+          validation: (Rule: { required: () => any; }) => Rule.required()
+        },
+        {
+          name: 'rows',
+          title: 'Rows',
+          type: 'array',
+          validation: (Rule: { required: () => any; }) => Rule.required(),
+          of: [{type: 'reference',to: [{type: 'pages'}]}]
+        }
+      ]
+    },
   ]
 }
